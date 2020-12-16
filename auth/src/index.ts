10ca81f6710b8b57d1e6180a90c175472b3dd407
chelsejw <chelsejw@gmail.com>
2020-12-16 11:22:57 +0800

    auth: finish kubernetes file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express'
-
 const app = express()
 app.use(express.json())
 const port = process.env.PORT || 3000
+
+
+app.get('/api/users/current', (req, res) => {
+  res.send('Hi there!')
+})
 
 app.listen(port, () => console.log(`Auth service is listening on port ${port}! :)`))
