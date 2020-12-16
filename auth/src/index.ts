f2364755a4c3045ea3ec66bec7be656483ff8087
chelsejw <chelsejw@gmail.com>
2020-12-16 10:55:03 +0800

    auth: move index.ts into src

---
+++ auth/src/index.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Auth service is listening on port ${port}`))
